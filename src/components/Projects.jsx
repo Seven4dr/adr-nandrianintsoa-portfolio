06a619e24e491633cf60cba6554635b79d7ee8df
nandrianintsoa <adrnandrianintsoa272@gmail.com>
2025-06-23 17:26:16 +0300

    v2 New design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { alarm2, dashboard, epi1, home, mobile} from '../../public/images';
