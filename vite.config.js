2435469af94cc002db22c0ba8de5932c83c5fc2e
nandrianintsoa <adrnandrianintsoa272@gmail.com>
2025-08-08 14:27:42 +0300

    v1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Pour un déploiement à la racine du domaine (user/organization page)
-  base: '/',
+  base: '/adr-nandrianintsoa.github.io/',
 })
