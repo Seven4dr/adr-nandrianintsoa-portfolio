c19741b17bfe6d35d0212d89b6902db66447f29b
nandrianintsoa <adrnandrianintsoa272@gmail.com>
2025-08-08 12:05:59 +0300

    Updates

---
+++ images/index.js
@@ -0,0 +1,63 @@
+import bluewhite from './bluewihte.png';
+import sary from './sary.png';
+import madigicom from './madigicom.png';
+import manasoa from './manasoa.jpg';
+import adobxd from './adobxd.png';
+import figma from './figma.png';
+import emihack from './emihack.jpeg';
+import home from './home.png';
+import produits from './produits.jpg';
+import mobile from './mobile.jpg';
+import dashboard from './dasboard.jpg';
+import order from './order.jpg';
+import annexe1 from './annexe_info1.png';
+import annexe2 from './annexe_info2.png';
+import epi1 from './epi1.jpg';
+import epi2 from './epi2.jpg';
+import epi3 from './epi3.jpg';
+import epi4 from './epi4.jpg';
+import portfolio from './portfolio.jpg';
+import bgheader from './bgheader.jpg';
+import alarm1 from './alarm1.jpg';
+import alarm2 from './alarm2.jpg';
+import alarm3 from './alarm3.jpg';
+import alarm4 from './alarm4.jpg';
+import cvFile from './cv.pdf'; // Added missing import statement
+import predictia from './predictia.jpg'; // Added missing import statement
+import robo from './robo.png';
+import Robo3D from './Robo3D.gif' // Added missing import statement
+import bi from './bi.svg' // Added missing import statement
+
+
+export {
+    bluewhite,
+    sary,
+    madigicom, 
+    manasoa, // Added missing comma
+    adobxd, 
+    emihack, 
+    figma,
+    home, 
+    produits,
+    mobile,
+    order, //
+    dashboard,
+    annexe1,
+    annexe2,
+    epi1,
+    epi2,
+    epi3,
+    epi4,
+    portfolio, 
+    bgheader,
+    alarm1,
+    alarm2,
+    alarm3,
+    alarm4,
+    cvFile,
+    predictia,
+    robo,
+    Robo3D,
+    bi
+
+};
